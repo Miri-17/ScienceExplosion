87b161ac5a8e0f465f477056c7a9789920b8ef8c
Miri <mirino.otsubo@outlook.com>
2024-08-23 22:10:58 +0900

    cut down on wastefulness

--- Assets/AppMain/Temp/Scripts/GameController.cs
+++ Assets/AppMain/Temp/Scripts/GameController.cs
@@ -1,50 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks.Triggers;
-using Unity.Collections;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
     public static GameController Instance { get; private set; }
 
-    // ある六角形の中心から右の六角形の中心までの長さ
-    public float _hexagonWidth = 0.74f;
-
-    // Startで  _puzzlesXY = new GameObject[_rowSize, _columnSize]; としたいが
-    // Puzzle.csでも呼び出すため、先に指定
-    [HideInInspector] public GameObject[,] puzzlesXY = new GameObject[17, 7];
+    private List<Puzzle> _selectedPuzzles = new List<Puzzle>();
+    private GameObject[,] _puzzlesXY;
     // ある六角形の中心から上の六角形の中心までの長さ
-    [HideInInspector] public float hexagonHeight = 0;
+    private float _hexagonHeight = 0;
     // ある六角形の中心と上の六角形の中心のずれ
-    [HideInInspector] public float adjustmentWidth = 0;
-    
+    private float _adjustmentWidth = 0;
+
     [SerializeField] private GameObject[] _puzzles = null;
     [SerializeField] private GameObject _puzzleBlack = null;
     [SerializeField] private int _rowSize = 17;
     [SerializeField] private int _columnSize = 7;
-    // 4つまでしか選択できなくする
     [SerializeField] private int _puzzleDestroyLimit = 4;
     [SerializeField] private LineRenderer _lineRenderer = null;
+    // ある六角形の中心から右の六角形の中心までの長さ
+    [SerializeField] private float _hexagonWidth = 0.74f;
 
-    // 選択中のパズルを保持
-    private List<Puzzle> _selectedPuzzles = new List<Puzzle>();
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+    }
 
     private void Start() {
-        Instance = this;
+        _puzzlesXY = new GameObject[_rowSize, _columnSize];
 
-        hexagonHeight = _hexagonWidth * Mathf.Sin(60.0f * Mathf.Deg2Rad);
-        adjustmentWidth = -_hexagonWidth * Mathf.Cos(60.0f * Mathf.Deg2Rad);
+        _hexagonHeight = _hexagonWidth * Mathf.Sin(60.0f * Mathf.Deg2Rad);
+        _adjustmentWidth = -_hexagonWidth * Mathf.Cos(60.0f * Mathf.Deg2Rad);
 
-        // _puzzlesXY = new GameObject[_rowSize, _columnSize];
         // ヴィクトリアの時だけ敵のパズル色を無くし、ヴィクトリアのパズルを配置
         if (GameDirector.Instance.BattleCharacterIndex == 8)
             _puzzles[GameDirector.Instance.EnemyCharacterIndex] = _puzzleBlack;
         CreatePuzzles();
-    }
-
-    private void Update() {
-        UpdateLineRenderer();
     }
 
     private void CreatePuzzles() {
@@ -52,10 +47,10 @@ public class GameController : MonoBehaviour {
             for (int j = 0; j < _columnSize; j++) {
                 int r = Random.Range(0, _puzzles.Length);
                 var puzzlePrefab = Instantiate(_puzzles[r]);
-                var x = _hexagonWidth * i + adjustmentWidth * Mathf.Abs(j % 2);
-                var y = hexagonHeight * j;
+                var x = _hexagonWidth * i + _adjustmentWidth * Mathf.Abs(j % 2);
+                var y = _hexagonHeight * j;
                 puzzlePrefab.transform.position = new Vector2(x, y);
-                puzzlesXY[i, j] = puzzlePrefab;
+                _puzzlesXY[i, j] = puzzlePrefab;
 
                 Puzzle _puzzle = puzzlePrefab.GetComponent<Puzzle>();
                 _puzzle.Row = i;
@@ -64,50 +59,92 @@ public class GameController : MonoBehaviour {
         }
     }
 
+    private void DestroyPuzzles(List<Puzzle> selectedPuzzles) {
+        foreach (var selectedPuzzle in selectedPuzzles) {
+            Destroy(selectedPuzzle.gameObject);
+            _puzzlesXY[selectedPuzzle.Row, selectedPuzzle.Column] = null;
+        }
+    }
+
+    private void TriggerFallCheck(List<int> affectedRows) {
+        foreach (var row in affectedRows) {
+           for (int j = 1; j < _columnSize; j++) {
+                var temp_j = j;
+                while (temp_j > 0) {
+                    if (_puzzlesXY[row, temp_j - 1] != null || _puzzlesXY[row, temp_j] == null)
+                        break;
+                    
+                    FallPuzzle(row, temp_j);
+                    temp_j--;
+                }
+            }
+        }
+    }
+
+    private void FallPuzzle(int row, int column) {
+        var puzzle = _puzzlesXY[row, column].GetComponent<Puzzle>();
+        _puzzlesXY[row, column] = null;
+        puzzle.Column--;
+        var x = _hexagonWidth * puzzle.Row + _adjustmentWidth * Mathf.Abs(puzzle.Column % 2);
+        var y = _hexagonHeight * puzzle.Column;
+        puzzle.gameObject.transform.position = new Vector2(x, y);
+        _puzzlesXY[row, column - 1] = puzzle.gameObject;
+    }
+
+    private void UpdateLineRenderer() {
+        var puzzleCount = _selectedPuzzles.Count;
+        if (puzzleCount >= 2) {
+            _lineRenderer.positionCount = puzzleCount;
+            for (int i = 0; i < puzzleCount; i++) {
+                _lineRenderer.SetPosition(i, _selectedPuzzles[i].transform.position);
+            }
+            _lineRenderer.gameObject.SetActive(true);
+        } else {
+            _lineRenderer.gameObject.SetActive(false);
+        }
+    }
+
     public void OnPuzzleDown(Puzzle puzzle) {
         _selectedPuzzles.Add(puzzle);
         puzzle.SetSelection(true);
+
+        UpdateLineRenderer();
     }
 
     public void OnPuzzleEnter(Puzzle puzzle) {
-        if (_selectedPuzzles.Count < 1) return;
-        Debug.Log(_selectedPuzzles.Count);
+        if (_selectedPuzzles.Count < 1)
+            return;
+        // Debug.Log(_selectedPuzzles.Count);
 
         if (puzzle.IsSelected) {
             if (_selectedPuzzles.Count >= 2 && _selectedPuzzles[_selectedPuzzles.Count - 2] == puzzle) {
-                Debug.Log("Hello!");
+                // Debug.Log("Hello!");
                 var removedPuzzle = _selectedPuzzles[_selectedPuzzles.Count - 1];
                 removedPuzzle.SetSelection(false);
                 _selectedPuzzles.Remove(removedPuzzle);
             }
         } else {
-            if (_selectedPuzzles.Count >= _puzzleDestroyLimit) return;
+            if (_selectedPuzzles.Count >= _puzzleDestroyLimit)
+                return;
+            
             var length = (_selectedPuzzles[_selectedPuzzles.Count - 1].transform.position - puzzle.transform.position).magnitude;
-            Debug.Log("length: " + length);
+            // Debug.Log("length: " + length);
             if (length < _hexagonWidth + 0.01f) {
                 _selectedPuzzles.Add(puzzle);
                 puzzle.SetSelection(true);
             }
         }
+
+        UpdateLineRenderer();
     }
     
     public void OnPuzzleUp(Puzzle puzzle) {
         DestroyPuzzles(_selectedPuzzles);
+        // 消えたパズルがある行
+        var affectedRows = _selectedPuzzles.Select(p => p.Row).Distinct().ToList();
         _selectedPuzzles.Clear();
-    }
+        TriggerFallCheck(affectedRows);
 
-    private void DestroyPuzzles(List<Puzzle> puzzles) {
-        foreach (var puzzle in puzzles)
-            Destroy(puzzle.gameObject);
-    }
-
-    private void UpdateLineRenderer() {
-        if (_selectedPuzzles.Count >= 2) {
-            _lineRenderer.positionCount = _selectedPuzzles.Count;
-            _lineRenderer.SetPositions(_selectedPuzzles.Select(puzzle => puzzle.transform.position).ToArray());
-            _lineRenderer.gameObject.SetActive(true);
-        } else {
-            _lineRenderer.gameObject.SetActive(false);
-        }
+        UpdateLineRenderer();
     }
 }
--- Assets/AppMain/Temp/Scripts/Puzzle.cs
+++ Assets/AppMain/Temp/Scripts/Puzzle.cs
@@ -2,25 +2,13 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Puzzle : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerUpHandler {
-    public bool IsSelected { get; private set; }
-
-    // add
-    private GameController _gameController;
+    [SerializeField] private SpriteRenderer _selectedSprite = null;
     [SerializeField] private int _row = 0;
     [SerializeField] private int _column = 0;
+
+    public bool IsSelected { get; private set; }
     public int Row { get => _row; set => _row = value; }
     public int Column { get => _column; set => _column = value; }
-
-    [SerializeField] private SpriteRenderer _selectedSprite;
-
-    // add
-    private void Start() {
-        _gameController = FindObjectOfType<GameController>();
-    }
-    private void Update() {
-        if (_column > 0 && _gameController.puzzlesXY[_row, _column - 1] == null)
-            FallPuzzle();
-    }
 
     public void OnPointerDown(PointerEventData eventData) {
         // print($"オブジェクト {name} がマウスダウンされたよ！");
@@ -35,18 +23,12 @@ public class Puzzle : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, 
         GameController.Instance.OnPuzzleUp(this);
     }
 
+    /// <summary>
+    /// パズルの選択状態を変更する
+    /// </summary>
+    /// <param name="isSelected"></param>
     public void SetSelection(bool isSelected) {
         IsSelected = isSelected;
         _selectedSprite.enabled = IsSelected;
-    }
-
-    private void FallPuzzle() {
-        _gameController.puzzlesXY[_row, _column] = null;
-        _column--;
-
-        var x = _gameController._hexagonWidth * _row + _gameController.adjustmentWidth * Mathf.Abs(_column % 2);
-        var y = _gameController.hexagonHeight * _column;
-        this.transform.position = new Vector2(x, y);
-        _gameController.puzzlesXY[_row, _column] = this.gameObject;
     }
 }
