ece92b2917d586870a8787ad319b0975cf829738
Miri <mirino.otsubo@outlook.com>
2024-10-16 06:31:42 +0900

    build an iOS application

--- Assets/AppMain/Scripts/General/CutsceneDB.cs
+++ Assets/AppMain/Scripts/General/CutsceneDB.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CutsceneDB", menuName = "ScriptableObjects/Cutscene Database")]
